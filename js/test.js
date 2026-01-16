d81d07cefd58e2c04c2616eb5cf7e4c0e3493c6e
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-01-16 16:02:16 +0200

    js new

--- js/test.js
+++ js/test.js
@@ -10,3 +10,10 @@ console.log(findNumber(2, 6, 5));
 console.log(findNumber(8, 17, 3));
 console.log(findNumber(6, 9, 4));
 console.log(findNumber(16, 35, 7));
+
+function createReversedArray() {
+  const args = Array.from(arguments);
+  return args.toReversed();
+}
+console.log(createReversedArray(1, 2, 3, 4, 5));
+console.log(createReversedArray("a", "b", "c", "d"));
