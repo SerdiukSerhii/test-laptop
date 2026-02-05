3a4a32fdcdd418dbad5b63acd1f151ef945519b8
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-02-06 00:10:22 +0200

    hw6 task-2

--- js/test.js
+++ js/test.js
@@ -521,52 +521,92 @@
 
 // !=========================== task-4 ==============================
 
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter((user) => user.gender === gender)
-    .reduce((total, user) => total + user.balance, 0);
-};
+// const getTotalBalanceByGender = (users, gender) => {
+//   return users
+//     .filter((user) => user.gender === gender)
+//     .reduce((total, user) => total + user.balance, 0);
+// };
 
-const clients = [
-  {
-    name: "Moore Hensley",
-    gender: "male",
-    balance: 2811,
-  },
-  {
-    name: "Sharlene Bush",
-    gender: "female",
-    balance: 3821,
-  },
-  {
-    name: "Ross Vazquez",
-    gender: "male",
-    balance: 3793,
-  },
-  {
-    name: "Elma Head",
-    gender: "female",
-    balance: 2278,
-  },
-  {
-    name: "Carey Barr",
-    gender: "male",
-    balance: 3951,
-  },
-  {
-    name: "Blackburn Dotson",
-    gender: "male",
-    balance: 1498,
-  },
-  {
-    name: "Sheree Anthony",
-    gender: "female",
-    balance: 2764,
-  },
-];
+// const clients = [
+//   {
+//     name: "Moore Hensley",
+//     gender: "male",
+//     balance: 2811,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     gender: "female",
+//     balance: 3821,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     gender: "male",
+//     balance: 3793,
+//   },
+//   {
+//     name: "Elma Head",
+//     gender: "female",
+//     balance: 2278,
+//   },
+//   {
+//     name: "Carey Barr",
+//     gender: "male",
+//     balance: 3951,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     gender: "male",
+//     balance: 1498,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     gender: "female",
+//     balance: 2764,
+//   },
+// ];
 
-console.log(getTotalBalanceByGender(clients, "male")); // 12053
+// console.log(getTotalBalanceByGender(clients, "male")); // 12053
 
-console.log(getTotalBalanceByGender(clients, "female")); // 8863
+// console.log(getTotalBalanceByGender(clients, "female")); // 8863
 
 // !=======================================================================
+
+// !=========================== task-2 ====================================
+
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = [...items];
+  }
+
+  getItems() {
+    return [...this.#items];
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// !==========================================================================
